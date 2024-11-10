1ecea55007ddc83aec33e33008542504b1f3eb95
Adam <adamkosinski631@gmail.com>
2024-11-10 00:01:00 -0500

    don't interpolate on first note

--- theremin.py
+++ theremin.py
@@ -17,7 +17,7 @@ CM_PER_SEMITONE = 1.5
 FREQ_AT_20_CM = 880
 
 
-def distance_measurement():
+def get_distance_cm():
     # Send 10us pulse to trigger
     GPIO.output(TRIG_PIN, True)
     time.sleep(0.00001)
@@ -45,27 +45,34 @@ GPIO.setup(ECHO_PIN, GPIO.IN)
 
 
 # create and boot the server
+# raspberry pi hardware doesn't support duplex
 s = Server(duplex=0, buffersize=1024).boot()
 s.amp = 0.5
+
 # start the audio server, and wait a bit so we don't get weird blips as starting-up artifacts
 s.start()
 time.sleep(0.1)
 
 # interpolating signals to control base frequency and volume
-freq = SigTo(value=FREQ_AT_20_CM, time=SENSOR_SAMPLE_PERIOD_SEC, init=FREQ_AT_20_CM)
+freq = SigTo(value=0, time=SENSOR_SAMPLE_PERIOD_SEC, init=0)
 volume = SigTo(value=0, time=SENSOR_SAMPLE_PERIOD_SEC, init=0)
 
 output = waveforms.basic(freq, volume)
 output.out()
 
 try:
+    n_measurements_out_of_range = 0
+    out_of_range = False
     while True:
-        distance_cm = distance_measurement()
+        distance_cm = get_distance_cm()
         print("distance cm:", distance_cm)
 
         # if out of range, turn off the sound
         if distance_cm > MAX_DIST_CM:
-            volume.setValue(0)
+            n_measurements_out_of_range += 1
+            if n_measurements_out_of_range >= 3:
+                out_of_range = True
+                volume.setValue(0)
         else:
             volume.setValue(1)
 
@@ -75,10 +82,15 @@ try:
             if AUTOTUNE:
                 semitones_delta = int(semitones_delta)
             
+            # if this is the first pitch measurement, don't interpolate
+            # (so that previous irrelevant pitch information doesn't get mixed in)
+            freq.setTime(0 if out_of_range else SENSOR_SAMPLE_PERIOD_SEC)
+
             freq.setValue(FREQ_AT_20_CM * 2**(semitones_delta / 12))
 
-            # bring volume up to 1 once we get our first measurement
-            volume.setValue(1)
+            out_of_range = False
+            n_measurements_out_of_range = 0
+
 
         time.sleep(SENSOR_SAMPLE_PERIOD_SEC)
 
