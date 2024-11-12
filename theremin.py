0fd4bc3765ec7fbcd7be44c00d518a09361d8870
Adam <adamkosinski631@gmail.com>
2024-11-12 16:45:17 -0500

    stop using loudness correction because of mysterious distortion problems for low freqs

--- theremin.py
+++ theremin.py
@@ -14,10 +14,29 @@ SENSOR_SAMPLE_PERIOD_SEC = 0.100
 AUTOTUNE = True
 MAX_DIST_CM = 80
 CM_PER_SEMITONE = 1.5
-FREQ_AT_20_CM = 880
+FREQ_AT_20_CM = 440
+
+
+# class to record a signal and fade in / fade out at the ends to prevent clicking
+class Recording:
+    fade_time = 0.1
+    
+    def __init__(self, signal, filename):
+        self.fader = Fader(fadein=self.fade_time, fadeout=self.fade_time)
+        faded_signal = Mix(signal, mul=self.fader)
+        self.rec = Record(faded_signal, filename)
+        self.fader.play()
+    
+    def stop_recording(self):
+        self.fader.stop()
+        # after fade out (plus some buffer time), stop the recording
+        Clean_objects(self.fade_time + 0.5, self.rec).start()
+        
 
 
 def get_distance_cm():
+    measure_start = time.time()
+
     # Send 10us pulse to trigger
     GPIO.output(TRIG_PIN, True)
     time.sleep(0.00001)
@@ -35,6 +54,9 @@ def get_distance_cm():
     us_elapsed = 1000000*(stop_time - start_time)
     distance_cm = us_elapsed / 58  # from datasheet
 
+    measure_end = time.time()
+    print("time to measure", measure_end - measure_start)
+
     return distance_cm
 
 
@@ -47,7 +69,7 @@ GPIO.setup(ECHO_PIN, GPIO.IN)
 # create and boot the server
 # raspberry pi hardware doesn't support duplex
 s = Server(duplex=0, buffersize=1024).boot()
-s.amp = 0.1
+s.amp = 0.3
 
 # start the audio server, and wait a bit so we don't get weird blips as starting-up artifacts
 s.start()
@@ -55,9 +77,9 @@ time.sleep(0.1)
 
 # interpolating signals to control base frequency and volume
 freq = SigTo(value=0, time=SENSOR_SAMPLE_PERIOD_SEC, init=0)
-volume = SigTo(value=0, time=SENSOR_SAMPLE_PERIOD_SEC, init=0)
+volume = SigTo(value=0.0, time=SENSOR_SAMPLE_PERIOD_SEC, init=0.0)
 
-output = waveforms.basic(freq, volume)
+output = waveforms.sine(freq, volume)
 output.out()
 
 try:
@@ -88,8 +110,9 @@ try:
             # set frequency
             freq.setValue(FREQ_AT_20_CM * 2**(semitones_delta / 12))
 
+            correction = waveforms.loudness_correction_factor(freq.value)
             set_vol = s.amp * volume.value * waveforms.loudness_correction_factor(freq.value)
-            print(f"{freq.value:.1f} Hz, amplitude {set_vol:.3f}")
+            print(f"{freq.value:.1f} Hz, amplitude {set_vol:.3f}, correction factor {correction:.3f}")
 
             out_of_range = False
             n_measurements_out_of_range = 0
--- waveforms.py
+++ waveforms.py
@@ -1,20 +1,32 @@
 from pyo import *
 
+def get_correction_factor(freq):
+    f = freq
+    numerator = 12194**2 * f**4
+    denominator = (f**2 + 20.6**2) * ((f**2 + 107.7**2) * (f**2 + 737.9**2))**0.5 * (f**2 + 12194**2)
+    R = numerator / denominator
+    # return unscaled version
+    return 1 / R
+
+loudness_low_freq_threshold = 200
+max_correction_factor = get_correction_factor(loudness_low_freq_threshold)
+
 # Function that returns the factor we need to multiply amplitude by,
 # to ensure an equal perceptual volume for various frequencies
 # This is based on A-weighting dB corrections
 def loudness_correction_factor(freq):
     # calculate amplitude scaling factor
-    f = freq
-    numerator = 12194**2 * f**4
-    denominator = (f**2 + 20.6**2) * ((f**2 + 107.7**2) * (f**2 + 737.9**2))**0.5 * (f**2 + 12194**2)
-    R = numerator / denominator
-    return 0.5 / R
+    unscaled = get_correction_factor(freq)
+    scaled = unscaled / max_correction_factor
+    clamped = min(0.8, scaled)
+    return clamped
 
 
-def basic(freq, volume):
-    high_harmonic = 4
+def standard(freq, volume):
+    high_harmonic = 3
     harmonics = [freq * i for i in range(1, high_harmonic)]
-    correction = loudness_correction_factor(freq) 
-    amps = [volume * correction * 0.33 / i for i in range(1, high_harmonic)]
+    amps = [volume * 0.33 / i for i in range(1, high_harmonic)]
     return Sine(freq=harmonics, mul=amps)
+
+def sine(freq, volume):
+    return Sine(freq=freq, mul=volume)
