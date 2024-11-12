a89610e568e2ccd6683deba5c62b993d1701dd87
Adam <adamkosinski631@gmail.com>
2024-11-12 17:03:33 -0500

    separate out sensor logic

--- theremin.py
+++ theremin.py
@@ -7,9 +7,8 @@ import time
 import numpy as np
 import RPi.GPIO as GPIO
 import waveforms
+import gpio
 
-TRIG_PIN = 24
-ECHO_PIN = 23
 SENSOR_SAMPLE_PERIOD_SEC = 0.100
 AUTOTUNE = True
 MAX_DIST_CM = 80
@@ -33,38 +32,7 @@ class Recording:
         Clean_objects(self.fade_time + 0.5, self.rec).start()
         
 
-
-def get_distance_cm():
-    measure_start = time.time()
-
-    # Send 10us pulse to trigger
-    GPIO.output(TRIG_PIN, True)
-    time.sleep(0.00001)
-    GPIO.output(TRIG_PIN, False)
-
-    # Measure pulse duration
-    start_time = time.time()
-    stop_time = time.time()
-    while GPIO.input(ECHO_PIN) == 0:
-        start_time = time.time()
-    while GPIO.input(ECHO_PIN) == 1:
-        stop_time = time.time()
-
-    # Calculate pulse duration (in seconds)
-    us_elapsed = 1000000*(stop_time - start_time)
-    distance_cm = us_elapsed / 58  # from datasheet
-
-    measure_end = time.time()
-    print("time to measure", measure_end - measure_start)
-
-    return distance_cm
-
-
-# Initialize GPIO
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(TRIG_PIN, GPIO.OUT)
-GPIO.setup(ECHO_PIN, GPIO.IN)
-
+gpio.init()
 
 # create and boot the server
 # raspberry pi hardware doesn't support duplex
@@ -86,7 +54,7 @@ try:
     n_measurements_out_of_range = 0
     out_of_range = False
     while True:
-        distance_cm = get_distance_cm()
+        distance_cm = gpio.get_distance_cm()
         print("distance cm:", distance_cm)
 
         # if out of range, turn off the sound
@@ -117,10 +85,9 @@ try:
             out_of_range = False
             n_measurements_out_of_range = 0
 
-
         time.sleep(SENSOR_SAMPLE_PERIOD_SEC)
 
 except KeyboardInterrupt:
     print("Keyboard interrupt, cleaning up GPIO and audio server")
-    GPIO.cleanup()
+    gpio.cleanup()
     s.stop()
