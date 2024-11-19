be362d9a2ad275635c858b75f7c1306777156694
Adam <adamkosinski631@gmail.com>
2024-11-19 16:25:40 -0500

    rewrite theremin as a class

--- theremin.py
+++ theremin.py
@@ -8,12 +8,13 @@ import numpy as np
 import RPi.GPIO as GPIO
 import waveforms
 import gpio
+import signal
 
 SENSOR_SAMPLE_PERIOD_SEC = 0.100
 AUTOTUNE = True
 MAX_DIST_CM = 80
 CM_PER_SEMITONE = 1.5
-FREQ_AT_20_CM = 440
+FREQ_AT_20_CM = 880
 
 
 # class to record a signal and fade in / fade out at the ends to prevent clicking
@@ -30,62 +31,75 @@ class Recording:
         self.fader.stop()
         # after fade out (plus some buffer time), stop the recording
         Clean_objects(self.fade_time + 0.5, self.rec).start()
-        
 
-gpio.init()
 
-# create and boot the server
-# raspberry pi hardware doesn't support duplex
-s = Server(duplex=0, buffersize=1024)
-s.setOutputDevice(1)  # so it works even when hdmi is plugged in
-s.amp = 0.3
-s.boot().start()
-
-# interpolating signals to control base frequency and volume
-freq = SigTo(value=0, time=SENSOR_SAMPLE_PERIOD_SEC, init=0)
-volume = SigTo(value=0.0, time=SENSOR_SAMPLE_PERIOD_SEC, init=0.0)
-
-output = waveforms.sine(freq, volume)
-output.out()
-
-try:
+class Theremin():
     n_measurements_out_of_range = 0
     out_of_range = False
-    while True:
-        distances_cm = gpio.get_distances(MAX_DIST_CM)
-        print("distances (cm):", distances_cm)
-        distance_cm = distances_cm[0]
+
+    def __init__(self):
+        gpio.init()
+
+        # create and boot the audio server
+        self.s = Server(duplex=0, buffersize=1024)
+        self.s.setOutputDevice(1)  # so it works even when hdmi is plugged in
+        self.s.amp = 0.3
+        self.s.boot().start()
+
+        # interpolating signals to control base frequency and volume
+        self.freq = SigTo(value=0, time=SENSOR_SAMPLE_PERIOD_SEC, init=0)
+        self.volume = SigTo(value=0.0, time=SENSOR_SAMPLE_PERIOD_SEC, init=0.0)
+        self.audio_signal = waveforms.sine(self.freq, self.volume)
+        self.audio_signal.out()
+    
+    def __del__(self):
+        print("\nCleaning up theremin GPIO and audio server")
+        gpio.cleanup()
+        self.s.stop()
+    
+    def sensor_update(self):
+        pitch_cm, volume_cm = gpio.get_distances(MAX_DIST_CM)
+        print(f"pitch {pitch_cm:.1f} cm, volume {volume_cm:.1f} cm")
 
         # if out of range, turn off the sound
-        if distance_cm > MAX_DIST_CM:
-            n_measurements_out_of_range += 1
-            if n_measurements_out_of_range >= 3:
-                out_of_range = True
-                volume.setValue(0)
+        if pitch_cm > MAX_DIST_CM or volume_cm > MAX_DIST_CM:
+            self.n_measurements_out_of_range += 1
+            if self.n_measurements_out_of_range >= 3:
+                self.out_of_range = True
+                self.volume.setValue(0)
         else:
-            volume.setValue(1)
+            self.volume.setValue(1)
 
             # if this is the first pitch measurement, don't interpolate
             # (so that previous irrelevant pitch information doesn't get mixed in)
-            freq.setTime(0 if out_of_range else SENSOR_SAMPLE_PERIOD_SEC)
+            # reads previous out_of_range value
+            self.freq.setTime(0 if self.out_of_range else SENSOR_SAMPLE_PERIOD_SEC)
+            # set out of range correctly
+            self.out_of_range = False
+            n_measurements_out_of_range = 0
 
             # calculate semitone delta from reference frequency (at 20cm), based on distance
             # closer to the sensor should be higher pitched, like real theremins
-            semitones_delta = (20 - distance_cm) / CM_PER_SEMITONE
+            semitones_delta = (20 - pitch_cm) / CM_PER_SEMITONE
             if AUTOTUNE:
                 semitones_delta = int(semitones_delta)
             # set frequency
-            freq.setValue(FREQ_AT_20_CM * 2**(semitones_delta / 12))
+            self.freq.setValue(FREQ_AT_20_CM * 2**(semitones_delta / 12))
 
-            amplitude_out = s.amp * volume.value
-            print(f"{freq.value:.1f} Hz, amplitude {amplitude_out:.3f}")
+            amplitude_out = self.s.amp * self.volume.value
+            print(f"{self.freq.value:.1f} Hz, amplitude {amplitude_out:.3f}")
 
-            out_of_range = False
-            n_measurements_out_of_range = 0
 
+def signal_handler(sig, frame):
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    # set up ctrl+C handler
+    signal.signal(signal.SIGINT, signal_handler)
+
+    theremin = Theremin()
+
+    while True:
+        theremin.sensor_update()
         time.sleep(SENSOR_SAMPLE_PERIOD_SEC)
-
-except KeyboardInterrupt:
-    print("Keyboard interrupt, cleaning up GPIO and audio server")
-    gpio.cleanup()
-    s.stop()
