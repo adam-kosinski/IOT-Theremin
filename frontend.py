a531c128e8531f5a58efa35c01ea849c8935dad7
Helian <helian@Helians-MacBook-Pro.local>
2024-11-24 17:07:15 -0500

    testing size 3

--- frontend.py
+++ frontend.py
@@ -16,7 +16,7 @@ class SoundDeviceController(QMainWindow):
     def initUI(self):
         # Main window settings
         self.setWindowTitle("Sound Device Controller")
-        self.resize(400, 300)
+        self.resize(300, 300)
 
         # Set main window background color
         palette = self.palette()
